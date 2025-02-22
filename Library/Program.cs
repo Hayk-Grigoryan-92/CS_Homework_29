0984327e57e41e7836c377709884edda3cf93252
Hayk-Grigoryan-92 <hayk.grigoryan992@gmail.com>
2025-02-22 17:08:39 +0400

    Clomplete homework 29

---
+++ Library/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Library library = new Library();
+            Member member = new Member("Aram", "student");
+            Book book = new Book("Samvel","S1","dramma","Raffi");
+            library.AddMember(member);
+            library.AddBook(book);
+            library.AddBorrowedBook(member, "Samvel", "Raffi");
+            library.DisplayMembers();
+            library.DisplayBooks();
+        }
+    }
+
+    abstract class LibraryEntity
+    {
+        public string Id { get; } = Guid.NewGuid().ToString();
+
+        public string Name { get; set; }
+
+        public LibraryEntity(string name)
+        {
+            Name = name;
+        }
+
+        abstract public void DisplayDetails();
+    }
+
+    class Book : LibraryEntity
+    {
+        public Book(string name, string iSBN, string category, string author) : base(name)
+        {
+            ISBN = iSBN;
+            Category = category;
+            Author = author;
+        }
+        public string Author {  get; set; }
+        public string ISBN { get; set; }
+        public string Category { get; set; }
+
+        public override void DisplayDetails()
+        {
+            Console.WriteLine($"{Author} | {Name} | {Category} | {ISBN} | {Id}");
+        }
+    }
+
+    class Member : LibraryEntity
+    {
+        public Member(string name, string memberType) : base(name)
+        {
+            MembershipType = memberType;
+        }
+        public string MembershipType {  get; set; }
+
+        public List<string> BorrowedBooks { get; set; } = new List<string>();
+
+        public override void DisplayDetails()
+        {
+            string borrowedBooks = BorrowedBooks.Count > 0 ? string.Join(", ", BorrowedBooks) : "No books borrowed";
+            Console.WriteLine($"{Name} | {MembershipType} | {borrowedBooks} | {Id}");
+        }
+      
+    }
+
+    class Library
+    {
+        List<Member> members = new List<Member>();
+        List<Book> books = new List<Book>();
+
+        public void AddBorrowedBook(Member member, string name, string author)
+        {
+            foreach (Book item in books)
+            {
+                if (item.Name == name || item.Author == author)
+                {
+                    member.BorrowedBooks.Add($"{name} by {author}");
+                    Console.WriteLine($"{member.Name} borrowed {name} by {author} {item.Id}");
+                    return; 
+                }
+            }
+            Console.WriteLine($"Book \"{name}\" by {author} not found or already borrowed.");
+        }
+        public void AddBook(Book book)
+        {
+            books.Add(book);
+        }
+
+        public void AddMember(Member member)
+        {
+            members.Add(member);
+        }
+
+        public void DisplayBooks()
+        {
+            foreach(Book el in books)
+            {
+                el.DisplayDetails();
+            }
+        }
+
+        public void DisplayMembers()
+        {
+            foreach (Member el in members)
+            {
+                el.DisplayDetails();
+            }
+        }
+    }
+}
---
+++ lesson29/Program.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson29
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose you game");
+            
+            Casino game = new Casino();
+            bool flag = true;
+
+            while (flag)
+            {
+                Bet bet = null;
+                Console.WriteLine("Press 1: Number bet | Press 2: Odd bet | Press 3: Even bet | Press 4: Exit");
+                if (!int.TryParse(Console.ReadLine(), out int option)) continue;
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("Choose you lucky number");
+                        if (!int.TryParse(Console.ReadLine(), out int chooseDise) || chooseDise < 1 || chooseDise > 6)
+                        {
+                            Console.WriteLine("Invalid number! Choose between 1 and 6.");
+                            continue;
+                        }
+                        Console.WriteLine("Please insert bet amount");
+                        if (!int.TryParse(Console.ReadLine(), out int amount)) continue;
+                        bet = new NumberBet(amount, chooseDise);
+                        game.PlaceBet(bet);
+
+                        break;
+                    case 2:
+                        Console.WriteLine("Please insert bet amount");
+                        if (!int.TryParse(Console.ReadLine(), out int amount2) || amount2 <= 0)
+                        {
+                            continue;
+                        }
+                        bet = new OddBet(amount2);
+                        game.PlaceBet(bet);
+                        break;
+                    case 3:
+                        Console.WriteLine("Please insert bet amount");
+                        if (!int.TryParse(Console.ReadLine(), out int amount3) || amount3 <= 0)
+                        {
+                            continue;
+                        }
+                        bet = new EvenBet(amount3);
+                        game.PlaceBet(bet);
+                        break;
+                    case 4:
+                        flag = false;
+                        continue;
+                }
+            }
+        }
+    }
+
+    abstract class Bet
+    {
+        public int Amaunt { get; set; }
+        public Bet(int money)
+        {
+            Amaunt = money;
+        }
+
+        private int _dice;
+
+        public int Dice
+        {
+            get
+            {
+                Random d = new Random();
+                _dice = d.Next(0, 7);
+                return _dice;
+            }
+        }
+        public abstract bool IsWinningBet();
+
+        public virtual int PayOut()
+        {
+            return Amaunt * 2;
+        }
+    }
+
+    class NumberBet : Bet
+    {
+        private int _number;
+
+        public int Number
+        {
+            get
+            {
+                return _number;
+            }
+            set
+            {
+                if (value > 0 || value < 7)
+                {
+                    _number = value;
+                }
+                else
+                {
+                    throw new Exception("Wrong number of dice");
+                }
+            }
+        }
+        public NumberBet(int money, int num) : base(money)
+        {
+            Number = num;
+        }
+
+        public override int PayOut()
+        {
+            return Amaunt * 6;
+        }
+
+        public override bool IsWinningBet()
+        {
+            Console.WriteLine($"Dice : {Dice}");
+            if (Dice == Number)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+
+    class OddBet : Bet
+    {
+        public OddBet(int money) : base(money)
+        {
+
+        }
+        public override bool IsWinningBet()
+        {
+            if (Dice % 2 == 1)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+
+    class EvenBet : Bet
+    {
+        public EvenBet(int money) : base(money)
+        {
+
+        }
+        public override bool IsWinningBet()
+        {
+            if (Dice % 2 == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+
+
+    class Casino
+    {
+        public int PlayerBalance { get; set; } = 100;
+        public void PlaceBet(Bet bet)
+        {
+            if (PlayerBalance > bet.Amaunt)
+            {
+                PlayerBalance -= bet.Amaunt;
+                if (bet.IsWinningBet())
+                {
+                    PlayerBalance += bet.PayOut();
+                    Console.WriteLine("Congrats!!! You WIN.)");
+                    Console.WriteLine($"Your balance is: {PlayerBalance}");
+                }
+                else
+                {
+                    Console.WriteLine("You lose, try again");
+                    Console.WriteLine($"Your balance is: {PlayerBalance}");
+                }
+            }
+            else
+            {
+                throw new Exception("Your ballance is too low");
+            }
+        }
+
+    }
+}
